3b4c8aee8dc2dddaf85315af3a37bbebd2fac13b
Kenji Rikitake <kenji.rikitake@acm.org>
2014-07-18 10:51:16 +0900

    Update files/patch-src_openssl_cert.cpp
    * Enforce explicit use of shared_ptr in tqsl_setCertificateStatus()

--- files/patch-src_openssl_cert.cpp
+++ files/patch-src_openssl_cert.cpp
@@ -1,6 +1,6 @@
 --- src/openssl_cert.cpp.orig	2014-07-12 13:18:13.000000000 -0700
 +++ src/openssl_cert.cpp	2014-07-17 16:36:04.000000000 -0700
-@@ -4214,7 +4214,7 @@
+@@ -4214,20 +4214,22 @@
  	for (ep = ellist.find("Cert"); ep != ellist.end(); ep++) {
  		if (ep->first != "Cert")
  			break;
@@ -9,21 +9,30 @@
  		if (rval.second && strtol(rval.first.c_str(), NULL, 10) == serial) {
  			exists = true;
  			break;
-@@ -4227,7 +4227,7 @@
+ 		}
+ 	}
+ 
+-	XMLElement cs("Cert");
++	shared_ptr<XMLElement> cs_p = make_shared<XMLElement>("Cert");
++	XMLElement cs = *cs_p;
+ 	cs.setPretext("\n  ");
+-	XMLElement se;
++	shared_ptr<XMLElement> se_p(new XMLElement);
++	XMLElement se = *se_p;
  	se.setPretext(cs.getPretext() + "  ");
  	se.setElementName("status");
  	se.setText(status);
 -	cs.addElement(se);
-+	cs.addElement(shared_ptr<XMLElement>(&se));
++	cs.addElement(se_p);
  
  	cs.setAttribute("serial", sstr);
  	cs.setText("\n  ");
-@@ -4235,7 +4235,7 @@
+@@ -4235,7 +4237,7 @@
  	if (exists)
  		ellist.erase(ep);
  
 -	sfile.addElement(cs);
-+	sfile.addElement(shared_ptr<XMLElement>(&cs));
++	sfile.addElement(cs_p);
  	sfile.setText("\n");
  	return tqsl_dump_cert_status_data(sfile);
  }
